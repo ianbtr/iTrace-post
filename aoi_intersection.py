6cf66f99e7d17ba8e5f7e4b82e9ccc13103c4d5c
ianbtr <32246517+ianbtr@users.noreply.github.com>
2019-06-07 10:51:33 -0400

    Refactored code to consolidate iTrace file processing

--- aoi_intersection.py
+++ aoi_intersection.py
@@ -4,7 +4,7 @@ USAGE: <python> aoi_intersection.py <image width> <image height> <code filepath>
             [line|token] <gaze data file> <x field> <y field> <Duration field>
             <Smoothing> <Threshold>
 
-NOTE: If <font size x> is listed as 1, character resolution is assumed and all other
+NOTE: If <font size x> is given as 1, character resolution is assumed and all other
     font size fields are disregarded.
 """
 
@@ -12,15 +12,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 import sys
-import json
-import numpy as np
-#import matplotlib.pyplot as plt
-from structs import Rect
-from scipy.ndimage import label
-from gaze_aoi import generate_gaze_aois
-from code_aoi import generate_code_aois, generate_char_aois
-#from matplotlib.patches import Rectangle
-#from matplotlib.collections import PatchCollection
+from code_aoi import *
 
 if len(sys.argv) != 16:
     print("USAGE: <python> aoi_intersection.py <image width> <image height> <code filepath> "
@@ -38,54 +30,6 @@ for item in img_width, img_height:
         raise e
 img_width, img_height = int(sys.argv[1]), int(sys.argv[2])
 
-# Compute code mask for character-resolved stimulus
-if sys.argv[4] == "1":
-    code_mask = generate_char_aois(img_width, img_height, *(sys.argv[3:10]))
-# For actual pixels:
-else:
-    code_mask = generate_code_aois(img_width, img_height, *(sys.argv[3:10]))
-#plt.imshow(code_mask)
-#plt.savefig("code_mask.png", dpi=200)
+fsize_x = sys.argv[4]
 
-# Compute gaze mask
-gaze_mask = generate_gaze_aois(img_width, img_height, *(sys.argv[10:]))
-#plt.imshow(gaze_mask)
-#plt.savefig("gaze_mask.png", dpi=200)
-
-# Merge masks
-mask_intersection = np.logical_and(code_mask, gaze_mask)
-
-# Identify regions
-all_labels, num_features = label(mask_intersection)
-
-# Create rectangles
-rectangles = list()
-#fig_rectangles = list()
-for label in range(1, num_features+1):
-    row_occurrences, col_occurrences = np.where(all_labels == label)
-    left_extent = min(col_occurrences)
-    right_extent = max(col_occurrences)
-    top_extent = min(row_occurrences)
-    bottom_extent = max(row_occurrences)
-
-    rectangles.append(Rect(left_extent, right_extent,
-                           top_extent, bottom_extent))
-
-"""
-    fig_rectangles.append(Rectangle((left_extent, top_extent),
-                                    right_extent - left_extent,
-                                    bottom_extent - top_extent,
-                                    fill=False))
-
-
-fig, ax = plt.subplots(1)
-pc = PatchCollection(fig_rectangles, alpha=0.5, edgecolor='w')
-ax.add_collection(pc)
-plt.imshow(all_labels, cmap='Spectral')
-plt.savefig('rects.png', dpi=200)
-"""
-
-# Dump rectangles in order of area
-rectangles.sort(key=lambda rect: rect.area(), reverse=True)
-rect_dict = list(map(lambda rect: rect.as_dict(), rectangles))
-print(json.dumps(rect_dict))
+print(get_aoi_intersection(img_width, img_height, sys.argv[3:]))
